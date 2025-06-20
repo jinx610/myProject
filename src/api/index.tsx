552a77da2d3b2f16608c750044f4f0516bbcbb10
jinx <jxiao0610@gamil.com>
2025-06-20 16:32:26 +0800

    feat baseUrl

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,5 +1,8 @@
+const baseUrl = import.meta.env.VITE_API_BASE;
+fetch(`${baseUrl}/xxx`)
+
 export async function sendChat(question: string) {
-  const res = await fetch('/api/users', {
+  const res = await fetch(`${baseUrl}/api/users`, {
     method: 'POST',
     body: JSON.stringify({ name: 'alice', email: '3211' }),
     headers: { 'Content-Type': 'application/json' }
@@ -8,7 +11,7 @@ export async function sendChat(question: string) {
 }
 
 export async function testPing(code: string) {
-  const res = await fetch('/api/ping', {
+  const res = await fetch(`${baseUrl}/api/ping`, {
     method: 'POST',
     body: JSON.stringify({ code: code }),
     headers: { 'Content-Type': 'application/json' }
